2133b3569d07e587c72521ccdbc5cb705bc742e8
Pau Pericay <ppericay@gisce.net>
2014-03-14 17:10:58 +0100

    Fix de les dates i optimització del codi.

--- sipers/parser.py
+++ sipers/parser.py
@@ -10,25 +10,42 @@ import ConfigParser
 import re
 from datetime import datetime
 
-# from rq.decorators import job
-#
-# from redis import Redis
-# conn = Redis()
-#
-# @job(queue='sips', connection=conn)
-# def test_redis(documents, db):
-#     pass
+import shutil
+import tempfile
+
+
+def parse_datetime(value, format='%Y%m%d'):
+    try:
+        res = datetime.strptime(value, format)
+    except:
+        res = value
+    return res
+
 
 class Parsejador(object):
 
-    def extract_file(self, tail, head):
+    mongodb = None
+    fitxer_conf = None
+    dictres = None
+    filecodificat = None
+
+    delimiter = None
+    classe = None
+    contador = None
+    num_fields = None
+    headers = None
+    descartar = None
+
+    midafitxer = None
+
+    def extreu_arxiu(self, tail, head, tmp_dir):
         arxiuex = open(head + '/' + tail, 'rb')
         z = zipfile.ZipFile(arxiuex)
+        #print "namelist:{}".format(z.namelist())
         for name in z.namelist():
-            outpath = head + '/'
-            z.extract(name, outpath)
+            z.extract(name, tmp_dir)
         arxiuex.close()
-        return {}
+        return True
 
     def afagaarxius(self, path):
         llista_arxius = []
@@ -39,76 +56,90 @@ class Parsejador(object):
                 llista_arxius.append(fitxer)
         return llista_arxius
 
-    def detecta_conf(self, arxiu, directori, numseg):
-        # Per ara suposo que tots son de ENDESA
-        head, tail = os.path.split(arxiu)
-        #Afago el tipus segons el nom del fitxer
-        #tipus = tail.split('.')[-3]
-        headers = ''
-        delimiter = ''
-        classe = ''
-        dictres = {}
-        print os.listdir("configs")
-        for fitxer in os.listdir("configs"):
-            conf = ConfigParser.RawConfigParser()
-            conf.readfp(open("configs/"+fitxer))
-            pattern = conf.get('parser', 'pattern')
-            delimiter = conf.get('parser', 'delimiter')
-            classe = conf.get('parser', 'class')
-            contador = conf.get('parser', 'contador')
-
-            if re.match(pattern, tail):
-                print "pattern {}".format(pattern)
-                # Camps del header
-                headers = conf.items('fields')
-
-                #print "read headers:{}".format(conf.items('fields'))
-
-                # Camps per borrar del header
-                descartar = conf.options('descartar')
-
-                #print "headers: {}, descartar: {}".format(headers, descartar)
-                # Extreure el fitxer
-                #print "head:{}, tail:{}, directori:{}".format(head, tail,
-                #                                              directori)
-                self.extract_file(tail, directori)
-                # Tinc de buscar el fitxer extret
-                pathex = conf.get('parser', 'pathex')
-
-                for fitex in os.listdir(directori + pathex + numseg + '/SORT/'):
-                    rutacompleta = directori + pathex + numseg + '/SORT/' + fitex
-                    print "rutacompleta:{}".format(rutacompleta)
-                    dictres[rutacompleta] = [headers, descartar, delimiter,
-                                             classe, contador]
-            else:
-                print "no fa match"
-        if not headers:
-            print "Error, no s'ha trobat cap coincidencia amb els fitxers de " \
-                  "configuració"
-        return dictres
-
     def insert_mongo(self, documents, collection):
         # Afegeixo les entrades
         collection.insert(documents)
         return {}
 
-    def importamongo(self, filecodificat, midafitxer, db, valors):
+    def get_available_conf(self):
+        """Aquesta funció retorna una llista amb tots els tipus de fitxers que
+           podem carregar"""
+        confs = []
+        for fitxer in os.listdir("configs"):
+            fparts = fitxer.split(".")
+            confs.append(fparts[0])
+        return confs
+
+    def detectaconf(self, arxiu):
+        head, tail = os.path.split(arxiu)
+
+        for fitxer in os.listdir("configs"):
+            conf = ConfigParser.RawConfigParser()
+            conf.readfp(open("configs/"+fitxer))
+            pattern = conf.get('parser', 'pattern')
+            # Coincideix el nom del fitxer amb camp de configuració
+            if re.match(pattern, tail):
+                self.fitxer_conf = fitxer
+
+        if not self.fitxer_conf:
+            print "Error, no s'ha trobat cap coincidencia amb els fitxers de " \
+                  "configuració"
+            return False
+        else:
+            return True
+
+    def load_conf(self, arxiu, directori):
+        head, tail = os.path.split(arxiu)
+        conf = ConfigParser.RawConfigParser()
+        conf.readfp(open("configs/"+self.fitxer_conf))
+
+        # Valors de la configuracio
+        self.delimiter = conf.get('parser', 'delimiter')
+        self.classe = conf.get('parser', 'class')
+        self.contador = conf.get('parser', 'contador')
+        self.headers = conf.items('fields')
+        self.descartar = conf.options('descartar')
+        try:
+            self.num_fields = conf.get('parser', 'num_fields')
+        except:
+            self.num_fields = False
+
+
+        # Crear directori temporal
+        try:
+            tmp_dir = tempfile.mkdtemp()
+            self.extreu_arxiu(tail, directori, tmp_dir)
+            # Tinc de buscar el fitxer extret
+            for (path, dirs, files) in os.walk(tmp_dir):
+                if files:
+                    self.filecodificat = codecs.open(path+'/'+files[0], "r",
+                                                     "iso-8859-15")
+                    self.midafitxer = os.stat(path+'/'+files[0]).st_size
+        finally:
+            try:
+                shutil.rmtree(tmp_dir)
+            except OSError as exc:
+                if exc.errno != 2:
+                    raise
+
+        return {}
+
+    def connectamongo(self):
+        # Connectar i escollir la bbdd
+        client = MongoClient()
+        # Base de dades
+        self.mongodb = client.openerp
+        return self.mongodb
+
+    def carregar_mongo(self):
         # Log per els errors de lectura
         flog = open("log.txt", "w")
         # Camps del conf
-        headerss = valors[0]
-        headers_conf = [h[0] for h in headerss]
-        valores = [h[1] for h in headerss]
+        headers_conf = [h[0] for h in self.headers]
+        valores = [h[1] for h in self.headers]
         vals = [v.split() for v in valores]
-        #Només utilitzare aquests valors en cas de que hi hagin posicions
-        if len(vals[0]) == 2:
-            vals_tipus = [v[0] for v in vals]
-            vals_apa = [v[1] for v in vals]
-
-        descartar = valors[1]
-        delimiter = valors[2]
-        classe = valors[3]
-        contador = valors[4]
+        vals_tipus = [v[0] for v in vals]
+        vals_apa = [v[1] for v in vals]
 
         # Contador de linies
         count = 0
@@ -116,100 +147,65 @@ class Parsejador(object):
         sumatori = 0
         #USUARI DE TEST QUE FEM SERVIR
         user = 'default'
-        # Número de camps del header
-        len_header = str(len(headerss))
 
         # Afago la coleccio que vull
-        if classe == 'giscedata_sips_ps':
-            collection = db.giscedata_sips_ps
-        elif classe == 'giscedata_sips_consums':
-            collection = db.giscedata_sips_consums
+        if self.classe == 'giscedata_sips_ps':
+            collection = self.mongodb.giscedata_sips_ps
+        elif self.classe == 'giscedata_sips_consums':
+            collection = self.mongodb.giscedata_sips_consums
         else:
             print "Error, no es troba la collection al conf"
+            raise SystemExit
 
         # Comprovo que la collecció estigui creada, si no la creo
-        if not db[contador].count():
-            db[contador].save({"_id": classe, "counter": 1})
+        if not self.mongodb[self.contador].count():
+            self.mongodb[self.contador].save({"_id": self.classe, "counter": 1})
 
         # llegeixo per tot el fitxer
-        while filecodificat.tell() < midafitxer:
-            linia = filecodificat.readline()
-            slinia = tuple(linia.split(delimiter))
+        while self.filecodificat.tell() < self.midafitxer:
+            linia = self.filecodificat.readline()
+            slinia = tuple(linia.split(self.delimiter))
             slinia = map(lambda s: s.strip(), slinia)
 
-            # Comprovar que el número de camps es el mateix que la del header
-            if len(slinia) == int(len_header):
-                # creo el dataset amb els headers del conf
-                data = tablib.Dataset(slinia, headers=headers_conf)
-                # Borro les claus que em surt l'arxiu de configuracio
-                for d in descartar:
-                    del data[d]
-                # Millores: posar la cadena de lambda al fitxer de conf
-                for h in headerss:
-                    if h[1] == 'float':
-                        data.add_formatter(h[0], lambda a: a and float(a) or 0)
-                    if h[1] == 'integer':
-                        #print "h[1]:{}, h[0]:{}".format(h[1], h[0])
-                        data.add_formatter(h[0],
-                                           lambda a:
-                                           a and int(a.replace(',', '')) or 0)
-                    # TO DO : afegir format datetime
-                    #         camp per boolean
-                    if h[1] == 'long':
-                        data.add_formatter(h[0], lambda a: a and long(a) or 0)
-                # creo el dict anomenat document per insertar al mongo
-                document = data.dict[0]
+            contadorlinia = 0
+            position = [eval(num, {"n": contadorlinia}) for num in vals_apa]
 
-                #Add incremental id to store vals
-                counter = db[contador].find_and_modify(
-                    {'_id': classe},
-                    {'$inc': {'counter': 1}})
+            for i in range(0, len(slinia), len(position)):
+                try:
+                    datal = [slinia[p] for p in position]
+                    data = tablib.Dataset(datal, headers=headers_conf)
 
-                # Update del index
-                document.update(
-                    {'id': counter['counter'],
-                     'create_uid': user,
-                     'create_date': datetime.now()}
-                )
-
-                self.insert_mongo(document, collection)
-            elif len(slinia) != int(len_header) and len(vals[0]) < 2:
-                print "Error en la dimensió de la linea, no coincideix amb" \
-                      " la dimensió dels camps del fitxer de configuració."
-                print "long de slinia: {} long de headerss: {}".format(
-                    len(slinia), len(headerss)
-                )
-                print "slinia:{}".format(slinia)
-                print "headerss:{}".format(headerss)
-            else:
-                contadorlinia = 0
-                position = [eval(num, {"n": contadorlinia}) for num in vals_apa]
-
-                for i in range(0, len(slinia), len(position)):
-                    data = [slinia[p] for p in position]
-                    data = tablib.Dataset(data, headers=headers_conf)
+                    if self.num_fields and len(datal) != int(self.num_fields):
+                        flog.write("Longitud de la fila {} incorrecte\n"
+                                   "len_data:{}, "
+                                   "self.num_fields:{}".format(count,
+                                                               len(datal),
+                                                               self.num_fields))
 
                     # Borro les claus que em surt l'arxiu de configuracio
-                    for d in descartar:
+                    for d in self.descartar:
                         del data[d]
 
                     # Millores: posar la cadena de lambda al fitxer de conf
-                    for h, v in zip(headerss, vals_tipus):
+                    for h, v in zip(self.headers, vals_tipus):
                         if v == 'float':
                             data.add_formatter(h[0],
                                                lambda a: a and float(a) or 0)
                         if v == 'integer':
                             data.add_formatter(h[0],
-                                               lambda a: a and int(a) or 0)
-                        # TO DO : afegir format datetime
+                                               lambda a:
+                                               a and int
+                                               (a.replace(',', '')) or 0)
+                        if v == 'datetime':
+                                    data.add_formatter(h[0], parse_datetime)
                         if v == 'long':
                             data.add_formatter(h[0],
                                                lambda a: a and long(a) or 0)
 
                     document = data.dict[0]
                     #id incremental
-                    counter = db[contador].find_and_modify(
-                        {'_id': classe},
+                    counter = self.mongodb[self.contador].find_and_modify(
+                        {'_id': self.classe},
                         {'$inc': {'counter': 1}})
 
                     # Update del index
@@ -222,14 +218,18 @@ class Parsejador(object):
 
                     # Inserto el document al mongodb
                     self.insert_mongo(document, collection)
+                except Exception as e:
+                    print "Error a la linia: {}".format(e.message)
+                    flog.write("Error a la fila {} , "
+                               "no s'ha processat\n".format(count))
 
-                    contadorlinia += 1
-                    position = [eval(num, {"n": contadorlinia}) for num in
-                                vals_apa]
+                contadorlinia += 1
+                position = [eval(num, {"n": contadorlinia}) for num in
+                            vals_apa]
 
             count += 1
             sumatori += len(linia)
-            tantpercent = float(sumatori) / midafitxer * 100.0
+            tantpercent = float(sumatori) / self.midafitxer * 100.0
             print "Completat: {} %".format(tantpercent)
 
         print "Numero de linies: {}".format(count)
@@ -237,56 +237,35 @@ class Parsejador(object):
 
         return {}
 
-    def parser_sips_endesa(self, arxiu_conf):
+    def parser(self, arxiu, directori, conf=False):
+        # Si ve conf comprovar que sigui una opció possible
+        if conf not in self.get_available_conf() or conf is False:
+            print "Error, aquest tipus no existeix"
+        # Si no passem cap configuracio predeterminada
+        if not conf:
+            if self.detectaconf(arxiu):
+                self.load_conf(arxiu, directori)
+                self.carregar_mongo()
 
-        arxiu, valors = arxiu_conf.popitem()
-
-        #print arxiu
-        filecodificat = codecs.open(arxiu, "r", "iso-8859-15")
-        midafitxer = os.stat(arxiu).st_size
-        #print 'Mida del fitxer: {}'.format(midafitxer)
-
-        # Connectar i escollir la bbdd
-        client = MongoClient()
-        # Base de dades
-        db = client.openerp
-        #db = client.test_database
-
-        self.importamongo(filecodificat, midafitxer, db, valors)
-
-        #print data.json
-        filecodificat.close()
-
-        return {}
+        return True
 
     def __init__(self):
         ####
         # Parser del fitxer de SIPS de Endesa
         ####
-        #arxiu = "/home/pau/Documents/sips/20131217 Endesa.zip"
-        #arxiu = "/home/pau/Documents/sips/SCEF/EXPT/" \
-        #        "INFPSCOM/INF/SEG01/SORT/S131213_backup"
-
-        #arxiu = "/home/pau/Documents/sips/prova/FECSA.INF.SEG01.ZIP"
-
-        #Descomprimir el zip arrel i processar tots els fitxer - WIP -
-        #self.extractzips(arxiu)
 
         #Afagar els arxius de un directori
-        directori = "/home/pau/Documents/sips/prova"
+        directori = "/home/pau/Documents/sips/prova3"
         llista_arxius = self.afagaarxius(directori)
-        print "llista_arxius: {}".format(llista_arxius)
+
         for arxiu in llista_arxius:
-            #print "Arxiu: {}".format(arxiu)
-            fparts = arxiu.split(".")
-            numseg = fparts[-2]
-            arxiu_conf = self.detecta_conf(arxiu, directori, numseg)
-            #print arxiu_conf
-            self.parser_sips_endesa(arxiu_conf)
 
-        #Detectar tipus d'arxiu
-        #header, descartar, rutacompleta = self.detecta_conf(arxiu)
+            if self.connectamongo():
+                self.parser(arxiu, directori)
 
-        #Parsejar l'arxiu
-        #self.parser_sips_endesa(rutacompleta, header, descartar)
-
+            ## Part que funciona (antiga)
+            # fparts = arxiu.split(".")
+            # numseg = fparts[-2]
+            # arxiu_conf = self.detecta_conf(arxiu, directori, numseg)
+            # #print arxiu_conf
+            # self.parser_sips_endesa(arxiu_conf)
