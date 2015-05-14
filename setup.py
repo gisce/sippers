4aa752d57796ed5f2ec1b571b9ac92804cba5ca0
Eduard Carreras <ecarreras@gisce.net>
2015-05-14 07:58:25 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='sippers',
-    version='',
-    packages=['sippers', 'sippers.configs'],
-    url='',
-    license='',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/gisce/sippers',
+    license='GPLv3',
     author='GISCE-TI, S.L.',
     author_email='devel@gisce.net',
     description='',
