de0b40f37ff8539e6b54818c94f445209b1c42fb
Eduard Carreras <ecarreras@gisce.net>
2015-05-26 09:48:25 +0200

    Add some documentation

---
+++ docs/index.rst
@@ -0,0 +1,29 @@
+.. Sippers documentation master file, created by
+   sphinx-quickstart on Tue May 26 08:48:18 2015.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Sippers's documentation!
+===================================
+
+version: |version|
+
+.. image:: http://ci.gisce.net/api/badge/github.com/gisce/sippers/status.svg?branch=master
+   :target: http://ci.gisce.net/github.com/gisce/sippers
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   api
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
--- sippers/file.py
+++ sippers/file.py
@@ -9,29 +9,48 @@ from sippers import logger
 
 
 class SipsFileStats(object):
+    """ Stats for a SIPS file
+    """
     def __init__(self, size):
+        """
+        :param size: Size of file
+        """
         self.st_size = size
         self.read = 0
 
     @property
     def size(self):
+        """Human readeable size of the SIPS file.
+        """
         return naturalsize(self.st_size)
 
     @property
     def progress(self):
+        """Progress in % of read content.
+        """
         return '{}%'.format(
             int((float(self.read) / float(self.st_size)) * 100)
         )
 
 
 class PackedSipsFileStats(SipsFileStats):
+    """Stats for a Packed SIPS file (with zip).
+    """
     def __init__(self, size, n_files):
+        """
+        :param size: Size of the file
+        :param n_files: Number of files packed
+        """
         super(PackedSipsFileStats, self).__init__(size)
         self.n_files = n_files
         self.idx_file = 0
 
     @property
     def progress(self):
+        """Progress in % and the number of files read.
+
+         Format example: 80% (8/10)
+        """
         return '{} ({}/{})'.format(
             super(PackedSipsFileStats, self).progress,
             self.idx_file, self.n_files
@@ -39,7 +58,25 @@ class PackedSipsFileStats(SipsFileStats):
 
 
 class PackedSipsFile(object):
+    """Packed SIPS file.
+
+    Process of content of zip file is processed with iterators to keep the
+    minimal memory footprint.
+
+    Example::
+
+        with PackedSipsFile('/tmp/PACKED.SIPS.zip') as packed:
+            for sips_file in packed:
+                for line in sips_file:
+                    print sips_file.stats.progress
+                    print line
+                print packed.stats.progress
+
+    """
     def __init__(self, path):
+        """
+        :param path: Packed SIPS file path
+        """
         self.path = path
         self.parser = get_parser(self.path)()
         if not zipfile.is_zipfile(self.path):
@@ -74,10 +111,28 @@ class PackedSipsFile(object):
         self.close()
 
     def close(self):
+        """Close the file descriptor.
+        """
         self.fd.close()
 
 
 class SipsFile(object):
+    """SIPS file.
+
+    Process of content of file is processed with iterators to keep the
+    minimal memory footprint.
+
+    Example::
+
+        with SipsFile('/tmp/SIPS.TXT') as sips_file:
+            for line in sips_file:
+                print sips_file.stats.progress
+                print line
+
+    :param path: Path of SIPS file
+    :param fd: File descriptor (use this if you have already opened the file)
+    :param parser: Force to use a parser
+    """
     def __init__(self, path, fd=None, parser=None):
         self.path = path
         if parser is None:
