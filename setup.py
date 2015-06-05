f1eef9ef454613a1d08e630ddc0a1958d82f6160
Eduard Carreras <ecarreras@gisce.net>
2015-06-05 10:16:45 +0200

    Bump to v1.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sippers',
-    version='0.2.0',
+    version='1.0.0',
     packages=find_packages(),
     url='https://github.com/gisce/sippers',
     license='GPLv3',
