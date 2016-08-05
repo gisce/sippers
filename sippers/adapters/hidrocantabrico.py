7113985cb4423611038a9fb251824e57fa5468d0
Arua Ibrahim <aibrahim@abenergia.es>
2016-08-05 12:33:29 +0200

    IMP fix incorrect dates

--- sippers/adapters/hidrocantabrico.py
+++ sippers/adapters/hidrocantabrico.py
@@ -75,6 +75,8 @@ class HidrocantabricoSipsAdapter(SipsAdapter, SipsSchema):
             if isinstance(field, fields.DateTime):
                 orig = data.get(attr)
                 if orig not in ('', '0', '00000000', '30000'):
+                    if orig[4:6] == '02' and orig[6:8] == '29':
+                        orig = orig[:6] + '28' + orig[8:]
                     data[attr] = '{}-{}-{}T00:00:00'.format(
                         orig[0:4], orig[4:6], orig[6:8]
                     )
