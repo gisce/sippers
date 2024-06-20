86672631b1ba1d05a36283158a174c90b446d94e
PSala <polsalalidon@gmail.com>
2024-06-20 13:15:59 +0200

    Bump to v2.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sippers',
-    version='1.1.1',
+    version='2.0.0',
     packages=find_packages(),
     url='https://github.com/gisce/sippers',
     license='GPLv3',
