43ea0838d2bcbe32690a28185e0e7d3caff45007
Release <github-actions@github.com>
2024-08-14 09:27:05 +0200

    Bump to v2.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sippers',
-    version='2.2.0',
+    version='2.3.0',
     packages=find_packages(),
     url='https://github.com/gisce/sippers',
     license='GPLv3',
